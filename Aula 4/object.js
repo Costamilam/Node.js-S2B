a46a1c121a470a5bf5ac0caa358a0b42f1f5bd18
= <=>
2018-04-26 17:06:57 -0300

    al

--- Aula 4/object.js
+++ Aula 4/object.js
@@ -19,3 +19,34 @@ console.log(conjunto.size);
 for(let elemento of conjunto) {
     console.log(elemento);
 }
+
+//Objeos
+let objeto = {
+    key: 'value',
+    set: function(name, value) {
+        return this[name] = value;
+    },
+    size: function() {
+        return Object.keys(objeto).length;
+    },
+    toString: function() {
+        let str = '{\n';
+
+        for(let elemento of Object.keys(objeto)) {
+            str += `    ${elemento}: ${this[elemento]},\n`
+        }
+
+        return `${str.trim(',\n', '')}\n}`;
+    }
+};
+
+// ---- MY TRIM() ----
+String.prototype.trim = function(oldValue, newValue) {
+    regex = new RegExp(`^[${oldValue}]+|[${oldValue}]+$`, 'g');
+    return this.replace(regex, newValue);
+}
+
+objeto.set('chave', 'valor')
+console.log(objeto.size());
+console.log(objeto.toString());
+
