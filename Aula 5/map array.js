b7081df1792fe5dc28439c85ed7adc4c91739f52
= <=>
2018-05-02 19:56:37 -0300

    all

---
+++ Aula 5/map array.js
@@ -0,0 +1,36 @@
+let array = [1, 0, 1, 2, 3, 3, 3, 4];
+
+console.log(' - - - Map Object - - -')
+
+let frequence = new Map();
+
+for(let element of array) {
+    if(frequence.has(element)) {
+        frequence.set(element, frequence.get(element) + 1);
+    } else {
+        frequence.set(element, 1);
+    }
+}
+
+for(let [key, value] of frequence) {
+    console.log(`the value ${key} repeat ${value} times`);
+}
+
+console.log(' - - - forEach  - - - ');
+
+let previousElement;
+
+array.sort().forEach(function(element, index) {
+    let count = 0;
+
+    array.sort().forEach(function(e, i) {
+        if(element == e) {
+            count++;
+        }
+    });
+
+    if(previousElement !== element) {
+        previousElement = element;
+        console.log(`the value ${element} repeat ${count} times`);
+    }
+});
