6aea3dbdbaa7e608cc88acc2b6b73204dde84866
= <=>
2018-05-08 15:58:08 -0300

    all

--- Aula 7/mongodb.js
+++ Aula 7/mongodb.js
@@ -2,23 +2,23 @@ const mongoClient = require('mongodb').MongoClient;
 const urlAccess = 'mongodb://localhost:27017';
 const databaseName = 'example';
 
-//Simples example of insert and select
+//Simples example of connection, insert and select
 (async function() {
-    let client;
+    let connection;
     try {
         //Connect in MongoDB
-        client = await mongoClient.connect(urlAccess);
+        connection = await mongoClient.connect(urlAccess);
         console.log('Connected successfully');
 
         //Database selected
-        const database = client.db(databaseName);
+        const database = connection.db(databaseName);
         console.log('Database selected');
 
         //Insert document in collection
         let result = await database.collection('example').insertOne({key: 'value'});
         console.log(`Inserted ${result.insertedCount} document into the collection. Document id: ${result.insertedId}`);
 
-        //Find Documents in collection
+        //Find document in collection
         let cursor = await database.collection('example').find({});
         console.log(`Find ${await cursor.count()} documents into the collection. Document values:`);
         cursor.forEach(function(element) {
